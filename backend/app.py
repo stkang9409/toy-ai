e56c210aac6f7c6d5e779a4119b671387b78ea9a
hoon25 <cchoon95@hanmail.net>
2023-04-16 13:04:38 +0900

    controller 정리

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 import logging
 
-from flask import Flask, request, session, jsonify, g
-from dotenv import load_dotenv
+from flask import Flask, request, g
 
-from core.dalle.dalle import fetch_image, fetch_image2, translate
 from core.chatGPT.chatGPT_service import *
 from common.config.load_config import get_flask_secret_key
 from common.util.session_user import set_user, get_user
@@ -28,13 +26,11 @@ def create_book():
     params = request.get_json()
     book = params['book']
     book_id = gpt_book_start(book)
-    
     return success_response({"book_id": book_id})
 
 @app.route('/book/<book_id>/<seq>', methods=['GET'])
 def book_content(book_id, seq):
     return success_response(get_book_content(book_id, seq))
-    # return success_response({"result": gpt_book_start(seq)})
 
 
 @app.route('/book/<book_id>/<seq>', methods=['POST'])
@@ -42,75 +38,7 @@ def save_user_content_choice(book_id, seq):
     params = request.get_json()
     user_candidate_num = params['candidate_num']
     save_next_book_content(book_id, seq, user_candidate_num)
-    
-    return "hi"
-    # return success_response({"result": gpt_book_start(seq)})
-
-
-@app.route('/dalle', methods=['GET'])
-def dalle():
-    resImage = fetch_image2()
-    return resImage
-
-
-@app.route('/test')
-def test():
-    msg = main()
-    return msg
-
-# [테스트]
-# @app.route('/db', methods=['GET'])
-# def db_test():
-#     connection = get_db_connection()
-#     cursor = connection.cursor()
-
-#     # Example query: get the MySQL server version
-#     cursor.execute("SELECT VERSION()")
-#     result = cursor.fetchone()
-
-#     connection.close()
-#     return jsonify({"mysql_version": result[0]})
-
-# @app.route('/tables/<table_name>', methods=['GET'])
-# def check_table(table_name):
-#     exists = table_exists(table_name)
-#     return jsonify({"table_name": table_name, "exists": exists})
-
-# [Generate Image]
-
-
-@app.route('/images', methods=['GET', 'POST'])
-def add_image():
-    if request.method == 'POST':
-        data = request.data.decode('utf-8')
-
-        if data:
-            translateData = translate(data)
-            english = translateData.choices[0].message.content
-            dalleResponse = fetch_image(english)
-
-            korean = translate(dalleResponse["data"][0]['url'])
-
-            # return jsonify({'status': 'success', 'message': 'Image URL added successfully', 'image_url':dalleResponse["data"][0]['url']})
-            return dalleResponse["data"][0]['url']
-        else:
-            return jsonify({'status': 'error', 'message': 'Image URL missing from request body'})
-    if request.method == 'GET':
-        connection = get_db_connection()
-        cursor = connection.cursor()
-
-        select_all_query = 'SELECT * FROM images'
-        cursor.execute(select_all_query)
-
-        images = []
-        for (id, image_url) in cursor:
-            images.append({'id': id, 'image_url': image_url})
-
-        cursor.close()
-        connection.close()
-
-        return jsonify({'images': images})
-
+    return success_response({})
 
 @app.teardown_appcontext
 def teardown_db(exception):
--- backend/common/util/response_type.py
+++ backend/common/util/response_type.py
@@ -1,4 +1,4 @@
 from flask import jsonify, make_response
 
-def success_response(data, msg="Success"):
+def success_response(data, msg="success"):
     return make_response(jsonify({"message": msg, "data": data}), 200)
